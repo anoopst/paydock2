11b033e3dea8f4ccd2f0c45df6791e7a8f83d678
anoopst <anoop.st@gmail.com>
2016-05-25 23:47:12 +0530

    initial commit

---
+++ src/main/java/com/sg/docker/poc/controller/DockerController.java
@@ -0,0 +1,24 @@
+package com.sg.docker.poc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DockerController { 
+
+	String message = "SpringMVC";
+	
+	@RequestMapping(value = "/hello",method=RequestMethod.GET)
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
